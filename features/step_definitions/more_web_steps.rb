b51ab95164479abe51ca081666289606f5cd2489
Adam Walters <ajwalters@gmail.com>
2010-08-29 11:55:46 -0500

    Hide external links if they're not present

--- features/step_definitions/more_web_steps.rb
+++ features/step_definitions/more_web_steps.rb
@@ -5,3 +5,11 @@ Then /^I should see an external link "([^"]*)"(?: to "([^"]*)")?$/ do |link_text
   link[:href].should == link_dest if link_dest
 end
 
+Then /^I should not see links:$/ do |links|
+  page_links = all('a')
+  page_links_text = page_links.map(&:text)
+  links.raw.each do |row|
+    page_links_text.should_not include(row.first)
+  end
+end
+
