8b35215918fafbf63db6738dc77f170893202e0f
Matt Polito <matt.polito@gmail.com>
2010-09-09 11:10:08 -0500

    updated flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
   def flash_messages
-    output = ""
-    output += content_tag(:div, :id => 'flash_messages') do
+    content_tag(:div, :id => 'flash_messages') do
+      output = ""
       flash.each do |type, msg|
         output += content_tag(:p, msg, :class => "message #{type.to_s}")
       end
-      output
+      raw output
     end
-    raw output
   end
-
 end
