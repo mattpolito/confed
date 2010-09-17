124b434633f78ed2a091e7d670dfa8779a6e70ff
Matt Polito <matt.polito@gmail.com>
2010-09-17 11:23:16 -0500

    made some seed data to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,26 @@
 # Create dummy Speakers
 ['Matt Polito', 'Adam Walters', 
   'Stephen Korecky', 'Chris Hallendy'].each do |speaker|
-  Speaker.create!(
+  speaker = Speaker.create!(
     :name => speaker,
     :email => "#{speaker.parameterize('_')}@example.net"
   )
+  p "Speaker #{speaker.name} created"
 end
 
 # Create dummy presentations
 Speaker.all.each_with_index do |speaker, i|
-  Presentation.create!(
+  presentation = Presentation.create!(
     :title => "Presentation Title ##{i+1}",
     :speaker => speaker,
     :event_name => "WindyCityRails 2010"
   )
+  p "Presentation #{presentation.title} created"
 end
+
+# Create dummy user
+user = User.create! :email => 'd@mnit.com',
+         :password => 'password',
+         :password_confirmation => 'password'
+p "User #{user.email} created" 
+ 
