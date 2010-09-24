c37395069d87ea9b29ea8d4b12578b4d6a3fd4d8
Matt Polito <matt.polito@gmail.com>
2010-09-23 22:17:43 -0500

    event association to get speakers through presentations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   # Assocations
   has_many :presentations
+  has_many :speakers, :through => :presentations
 
   # Validations
   validates :name, :presence => true
