cb1b3f67617b4fe6cb9737d148f7e0e631cbb6da
Matt Polito <matt.polito@gmail.com>
2010-09-24 18:01:52 -0500

    fixed default gravatar image

--- config/initializers/gravatarify.rb
+++ config/initializers/gravatarify.rb
@@ -1,3 +1,3 @@
-Gravatarify.options[:default] = Proc.new { |*params| "localhost:3000/images/default_avatar_#{params.first[:size] || 80}.png" }
+Gravatarify.options[:default] = Proc.new { |*params| "http://localhost:3000/images/default_avatar_#{params.first[:size] || 80}.png" }
 Gravatarify.options[:html] = { :class => 'gravatar' }
 
