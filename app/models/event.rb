76f5c82a162a6844cc2c7b48f8cf86d13f8e3468
Matt Polito <matt.polito@gmail.com>
2010-09-24 17:57:27 -0500

    added search on event name

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,11 @@ class Event < ActiveRecord::Base
   has_many :presentations
   has_many :speakers, :through => :presentations
 
+  # Full text indexing
+  index do
+    name 'A'
+  end
+
   # Validations
   validates :name, :presence => true
 end
