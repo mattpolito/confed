08b0430a6cdea469182f8abf37923e2b88d968eb
Matt Polito <matt.polito@gmail.com>
2010-09-29 08:25:37 -0500

    refactor of presentation index feature

--- features/step_definitions/presentation_steps.rb
+++ features/step_definitions/presentation_steps.rb
@@ -66,18 +66,6 @@ Then /^I should see (\d+) presentation listing(s)?$/ do |number, arg2|
   end
 end
 
-Then /^it should display title as a link$/ do
-    page.should have_css('.presentation a.title')
-end
-
-Then /^it should display description$/ do
-  page.should have_css('.presentation .description')
-end
-
-Then /^it should display presenter's name$/ do
-  page.should have_css('.presentation .speaker')
-end
-
 Then /^it should display date it took place$/ do
   page.should have_css('.presentation .took_place_on')
 end
