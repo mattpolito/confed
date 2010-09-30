bfa3758be731814c28a14ac40a96a3dda1333f1f
Matt Polito <matt.polito@gmail.com>
2010-09-30 10:41:44 -0500

    speakers routing spec was testing admin and not public

--- spec/routing/speakers_routing_spec.rb
+++ spec/routing/speakers_routing_spec.rb
@@ -1,17 +1,13 @@
 require "spec_helper"
 
-describe Admin::SpeakersController do
+describe SpeakersController do
   describe "routing" do
-    it "recognizes and generates admin #index" do
-      { :get => "/admin/speakers" }.should route_to(:controller => "admin/speakers", :action => "index")
-    end
-
-    it "recognizes and generates admin #new" do
-      { :get => "/admin/speakers/new" }.should route_to(:controller => "admin/speakers", :action => "new")
+    it "recognizes and generates #index" do
+      { :get => "/speakers" }.should route_to(:controller => "speakers", :action => "index")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/admin/speakers/1" }.should route_to(:controller => "admin/speakers", :action => "show", :id => "1")
+      { :get => "/speakers/1" }.should route_to(:controller => "speakers", :action => "show", :id => "1")
     end
   end
 end
