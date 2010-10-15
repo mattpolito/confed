2c80a425cc2f6fb3c415781cd9fcbc6679d5d967
Adam Walters <ajwalters@gmail.com>
2010-10-15 15:31:37 -0500

    add overridable max_width to embedly calls

--- app/models/embedly.rb
+++ app/models/embedly.rb
@@ -1,6 +1,8 @@
 class Embedly
+  DEFAULT_MAX_WIDTH = 640
   def self.get_attrs(url, options = {})
-    embedly_url = "http://api.embed.ly/v1/api/oembed?url=#{url}"
+    max_width = options[:max_width] || DEFAULT_MAX_WIDTH
+    embedly_url = "http://api.embed.ly/v1/api/oembed?url=#{url}&maxwidth=#{max_width}"
     response = HTTParty.get embedly_url
     response.parsed_response
   rescue
