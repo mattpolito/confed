edc531ccf9ded9769cc03321f3f90fcb484dea07
Matt Polito <matt.polito@gmail.com>
2010-10-26 18:07:23 -0500

    changed max default for embeds added provider check for speaker deck on slideshows

--- app/models/embedly.rb
+++ app/models/embedly.rb
@@ -1,5 +1,5 @@
 class Embedly
-  DEFAULT_MAX_WIDTH = 550
+  DEFAULT_MAX_WIDTH = 592
   def self.get_attrs(url, options = {})
     max_width = options[:max_width] || DEFAULT_MAX_WIDTH
     embedly_url = "http://api.embed.ly/v1/api/oembed?url=#{url}&maxwidth=#{max_width}"
