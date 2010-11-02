801292d22103c7ad169dc45af363700b63042e04
Matt Polito <matt.polito@gmail.com>
2010-11-02 08:12:36 -0500

    fixed broken embedly spec with incorrect default max width

--- spec/models/embedly_spec.rb
+++ spec/models/embedly_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Embedly do
-  describe 'get_attrs' do
-    it 'accpets an optional max_width parameter passed to Emebedly' do
+  describe '.get_attrs' do
+    it 'accepts an optional max_width parameter passed to Emebedly' do
       HTTParty.should_receive(:get).with(/maxwidth=200/)
       Embedly.get_attrs("http://foo.com", :max_width => 200)
     end
 
-    it 'defaults to a max_width of 640' do
-      HTTParty.should_receive(:get).with(/maxwidth=640/)
+    it 'defaults to a max_width of 592' do
+      HTTParty.should_receive(:get).with(/maxwidth=592/)
       Embedly.get_attrs("http://foo.com")
     end
   end
