c4bf6e0e70b108bab046867dddd4ea1e586ee81b
Matt Polito <matt.polito@gmail.com>
2010-11-18 18:04:07 -0600

    ignoring sass generated css files and added in some files that should be in repo

---
+++ db/schema.rb
@@ -0,0 +1,123 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20101111234041) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "started_on"
+    t.datetime "ended_on"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "external_embeds", :force => true do |t|
+    t.string   "url"
+    t.text     "content",         :null => false
+    t.string   "type",            :null => false
+    t.integer  "presentation_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "thumbnail"
+  end
+
+  create_table "presentations", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "video"
+    t.string   "event_name"
+    t.datetime "took_place_on"
+    t.integer  "speaker_id"
+    t.string   "tag_cache"
+    t.integer  "event_id"
+    t.text     "rendered_description"
+  end
+
+  add_index "presentations", ["speaker_id"], :name => "index_presentations_on_speaker_id"
+
+  create_table "presentations_speakers", :id => false, :force => true do |t|
+    t.integer "presentation_id"
+    t.integer "speaker_id"
+  end
+
+  add_index "presentations_speakers", ["presentation_id", "speaker_id"], :name => "index_presentations_speakers_on_presentation_id_and_speaker_id", :unique => true
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  create_table "speakers", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.string   "location"
+    t.text     "bio"
+    t.string   "company_name"
+    t.string   "company_url"
+    t.string   "email"
+    t.string   "twitter"
+    t.string   "website"
+    t.string   "speaker_rate_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "authentication_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+end
