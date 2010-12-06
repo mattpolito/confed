178bc20a67b2a005b5bb47e9c8a45b4e1f76e50d
Matt Polito <matt.polito@gmail.com>
2010-12-06 07:57:31 -0600

    atom feed for tags on presentation show page

--- app/controllers/tagged_controller.rb
+++ app/controllers/tagged_controller.rb
@@ -1,5 +1,16 @@
 class TaggedController < ApplicationController
   def show
     @presentations = Presentation.tagged_with(params[:tag_name])
+
+    respond_to do |format|
+      format.html
+      format.atom do
+        render 'shared/feed', :locals => {
+          :feed_title => "Presentations tagged with #{params[:tag_name]} - Confed",
+          :feed_updated_at => @presentations.first.created_at,
+          :presentations => @presentations
+        }
+      end
+    end
   end
 end
