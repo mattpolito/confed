7d343cc3af20052aff0c75b45ef8fb10909f8053
Matt Polito <matt.polito@gmail.com>
2010-12-30 07:24:36 -0600

    defining middleware to handle redirects for - redirecting back to main uri if not - non www uri

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # Rescuing
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
