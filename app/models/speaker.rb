aad687d2afdbcb61ac17e0b7f4b93732fbdf43de
Matt Polito <matt.polito@gmail.com>
2011-01-15 11:52:11 -0600

    completeness scoring on presentation, events, and speakers for admin view

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,6 +1,19 @@
 class Speaker < ActiveRecord::Base
   # Extensions
   has_friendly_id :name, :use_slug => true, :approximate_ascii => true
+  define_completeness_scoring do
+    check :name, lambda { |per| per.name.present? }, :high
+    check :title, lambda { |per| per.title.present? }, :low
+    check :location, lambda { |per| per.location.present? }, :low
+    check :bio, lambda { |per| per.bio.present? }, :medium
+    check :company_name, lambda { |per| per.company_name.present? }, :low
+    check :company_url, lambda { |per| per.company_url.present? }, :low
+    check :email, lambda { |per| per.email.present? }, :medium
+    check :twitter, lambda { |per| per.twitter.present? }, :medium
+    check :website, lambda { |per| per.website.present? }, :medium
+    check :speaker_rate_id, lambda { |per| per.speaker_rate_id.present? }, :low
+    check :presentations, lambda { |per| per.presentations.present? }, :high
+  end
 
   # Callbacks
   before_save :render_bio
