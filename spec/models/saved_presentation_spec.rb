368bf5a862db8c4f06ed420f5c73a629bb4aa284
Matt Polito <matt.polito@gmail.com>
2011-02-21 15:49:46 -0600

    [FEATURE]: Add to Watch list : Adam Walters & Matt Polito

--- spec/models/saved_presentation_spec.rb
+++ spec/models/saved_presentation_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe SavedPresentation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:presentation) }
+  it { should belong_to(:user) }
 end
